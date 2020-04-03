9b0f6a3f6a5d9285e66760a7d25d0a2403afe443
kriss riekstins <gigaminergaming@gmail.com>
2020-04-03 17:01:29 +0300

    Version.3

--- MyApp.cs
+++ MyApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GitHub
 {
-    public partial class Form1 : Form
+    public partial class Myapp : Form
     {
-        public Form1()
+        public Myapp()
         {
             InitializeComponent();
         }
